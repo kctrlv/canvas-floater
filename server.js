e319d46991ab53dafeaa3e3fc64bf9a07b1f5607
David Davydov <kctrlv@gmail.com>
2017-01-15 11:28:56 -0700

    server running

---
+++ server.js
@@ -0,0 +1,21 @@
+const http    = require('http');
+const express = require('express');
+const app     = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+var server = http.createServer(app);
+
+var port = process.env.PORT || 2115;
+
+var server = http.createServer(app);
+
+server.listen(port, function() {
+  console.log("Listening on port " + port + ".")
+})
+
+module.exports = server;
